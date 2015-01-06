889b58c8bb88cda9668fafd0af57c0ac13fc4598
Javier Abadia <javi.abadia@gmail.com>
2015-01-07 00:08:09 +0100

    made reading data code clearer

--- plot1.R
+++ plot1.R
@@ -3,11 +3,15 @@
 # ensure day and month names are written in English
 Sys.setlocale(locale='C')
 
-cols <- read.csv('../household_power_consumption.txt',sep=';',nrows=1);
-d <- read.csv('../household_power_consumption.txt',sep=';',nrows=24*60*2,skip=66636,header=T); 
+# read data
+nrows <- 24*60*2 # 2 days in minutes
+skiprows <- 66636 # row where 1/2/2007 00:00:00 appears
+cols <- read.csv('../household_power_consumption.txt',sep=';',nrows=1); # to get col names
+d <- read.csv('../household_power_consumption.txt',sep=';',nrows=nrows,skip=skiprows,header=T); # read data of first 2 days of Feb 2007
 names(d) <- names(cols); 
-d$DateTime <- strptime(paste(d$Date,d$Time),format="%d/%m/%Y %H:%M:%S")
+d$DateTime <- strptime(paste(d$Date,d$Time),format="%d/%m/%Y %H:%M:%S") # parse date-times
 
+# generate plot
 png(filename='plot1.png', width=480, height=480)
 hist(d$Global_active_power,col="red",xlab="Global Active Power (kilowatts)", main="Global Active Power")
 dev.off()
